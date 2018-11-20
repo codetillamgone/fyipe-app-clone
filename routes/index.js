ef4a1541f4a1b9f19fe373aafc46495c265be624
Adebiyi David <adebiyi137@gmail.com>
2018-11-20 15:20:33 +0100

    new commit for task check 1

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 
-var postData = {data : "MyData", condition:true};
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', condition:false, status:"success" });
+  res.render('index', { title: 'Express'});
 });
 
+//POST REQUEST 
 router.post('/data', function(req, res, next){
- res.redirect('/data/', postData);
+  
 });
+
+//GET REQUEST
+router.get('/data', function(req, res, next){
+
+});
+
 
 module.exports = router;
+ 
